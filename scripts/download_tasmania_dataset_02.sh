3717dc8fd1e7c48f89b3ea3775cf27fe9eb23893
Martin Kvisvik Larsen <martin.kvisvik.larsen@hotmail.com>
2022-08-31 09:57:40 +0200

    Update Tasmania 2 download script.

--- scripts/download_tasmania_dataset_02.sh
+++ scripts/download_tasmania_dataset_02.sh
@@ -1,4 +1,4 @@
 python src/squidle_parser.py \
-    tasmania_box_20 \
+    tasmania_02 \
     /home/martin/pCloudDrive/research/data/visual_place_recognition/collections/tasmania_box_20.csv \
-    /home/martin/pCloudDrive/research/data/imos/squidle/tasmania_box_20
+    /home/martin/pCloudDrive/research/data/visual_place_recognition/tasmania_02
